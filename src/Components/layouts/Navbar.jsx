bdcdf386bad6ec0a7a9819e184a6b644d814bc26
Modibo <tmod65926@gmail.com>
2026-04-09 17:16:17 +0200

    finition navbar

--- src/Components/layouts/Navbar.jsx
+++ src/Components/layouts/Navbar.jsx
@@ -1,29 +1,45 @@
+import Button from "../ui/button";
+
 const Navbar = () => {
+  const links = [
+    { label: "Accueil", href: "#accueil" },
+    { label: "Qui sommes-nous ?", href: "#about" },
+    { label: "Nos activités", href: "#activites" },
+    { label: "Adhésion", href: "#adhesion" },
+    { label: "Contact", href: "#contact" },
+  ];
+
   return (
-    <nav className="fixed top-0 w-full z-50 transition all duration-300 ">
-      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-10">
-        <div className="flex items-center justify-between h-20 md:h-24">
-            <a href="">
-                <img
-                  src="/Logo.png"
-                  alt="Logo AESMALY"
-                  className="h-14 sm:h-16 md:h-20 w-auto object-contain"
-                />
-            
-                <div className="sm:flex flex-col leading-tight">
-                  <span className="text-xl md:text-2xl font-semibold text-green-900 tracking-wide">
-                    AESMALY
-                  </span>
-                  <span className="text-[10px] md:text-xs text-gray-700 max-w-35">Association des Étudiants et Jeunes Maliens de Lyon</span>
-                </div>
-            </a>
-          </div>
-          <div className="flex items-center space-x-4 text-gray-700 group-hover:text-gray-900 transition duration-300">
-            <a href="/">Acceuil</a>
-            <a href="/about">Qui sommes-nous ?</a>
-            <a href="/Adhesion">Adhésion</a>
-            <a href="/contact">Contact</a>
-            <a href="/Adherer">Adhérer</a>
+    <nav className="top-0 w-full z-50 transition all duration-300 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          <a href="" className="flex items-center space-x-3  shrink-0">
+            <img
+              src="/Logo.png"
+              alt="Logo AESMALY"
+              className="h-14 sm:h-16 md:h-20 w-auto object-contain"
+            />
+
+            <div className="sm:flex flex-col leading-tight mt-4">
+              <span className="text-xl md:text-2xl font-semibold text-green-800 tracking-wide">
+                AESMALY
+              </span>
+              <span className="text-[10px] md:text-xs text-gray-700 max-w-35">
+                Association des Étudiants et Jeunes Maliens de Lyon
+              </span>
+            </div>
+          </a>
+          <div className="flex items-center space-x-5 text-gray-700 group font-semibold hover:text-gray-900 transition duration-300">
+            {links.map((link) => (
+              <a
+                key={link.label}
+                href={link.href}
+                className="text-sm md:text-base font-medium text-gray-700 hover:text-green-800 transition-colors relative after:absolute after:bottom-0 after:left-0 after:w-0  after:h-0.5 after:bg-green-800 after:transition-all after:duration-300 hover:after:w-full"
+              >
+                {link.label}
+              </a>
+            ))}
+            <Button/>
           </div>
         </div>
       </div>
